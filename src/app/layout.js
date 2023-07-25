81727c48e0297d8a257e20f08c97e9a4530e7506
Sakib-Atreus <sakibatreus@gmail.com>
2023-07-26 00:12:01 +0600

    title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
           href="https://freepngimg.com/save/158223-photos-mall-shopping-store-free-clipart-hq/512x512"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>My page title</title>
+        <title>Shop House</title>
       </head>
 
       <body className={inter.className}>
