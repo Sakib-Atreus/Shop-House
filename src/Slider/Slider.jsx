6521d5637299f93cf4ba06b850db1f13a4f4e97a
Sakib-Atreus <sakibatreus@gmail.com>
2023-07-26 00:08:03 +0600

    project done

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Slider = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-1/2">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="https://www.jssor.com/premium/fashion/img/plaza.jpg" className="w-full" />
+                <img src="https://www.realkart.in/images/home/slide-2.jpg" className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide4" className="btn btn-circle">❮</a> 
                 <a href="#slide2" className="btn btn-circle">❯</a>
